75102b66b2502b4138f338448d5954247aedb7b3
Neutron <james.neushul@gmail.com>
2015-11-18 03:09:15 +0200

    UI Work

--- MTFApp/mtfapp/js/controllers.js
+++ MTFApp/mtfapp/js/controllers.js
@@ -1,6 +1,6 @@
-/* global mtfApp, angular, msgctl, setctl, segctl, umsgctl */
+/* global mtfApp, angular, msgctl, setctl, segctl, umsgctl, ussegctl, ussetctl, usfldctl, nmsgctl, nsegctl, nsetctl */
 
-mtfApp.controller('mtfCtl', function (dbService) {
+mtfApp.controller('mtfCtl', function ($scope, dbService, uiService) {
     var mtfctl = this;
     mtfctl.selNode = [];
     mtfctl.listSelected = [];
@@ -9,11 +9,20 @@ mtfApp.controller('mtfCtl', function (dbService) {
     mtfctl.setview = 'views/setView.html';
     mtfctl.segmentview = 'views/segmentView.html';
     mtfctl.fieldview = 'views/fieldView.html';
-    mtfctl.views=
-        {'msgs':'views/msgView.html',
-        'segs':'views/segmentView.html',
-        'sets':'views/setView.html',
-        'flds':'views/fieldView.html'};
+
+    mtfctl.views = {
+        'umsgs_ui': 'views/msgView.html',
+        'nmsgs_ui': 'views/msgView.html',
+        'usegments_ui': 'views/segmentView.html',
+        'nsegments_ui': 'views/segmentView.html',
+        'usets_ui': 'views/setView.html',
+        'nsets_ui': 'views/setView.html',
+        'ufields_ui': 'views/fieldView.html',
+        'nfields_ui': 'views/fieldView.html'};
+    dbService.syncResources(uiService, mtfctl, function () {
+        //
+        console.log("Sync complete");
+    });
     mtfctl.valuefilter = function (mlist, txt, field) {
         var result = {
         };
@@ -30,76 +39,49 @@ mtfApp.controller('mtfCtl', function (dbService) {
         });
         return result;
     };
-    mtfctl.selectNode = function (list, node, k) {
+    mtfctl.listfilter = function (mlist, txt) {
+        var result = {
+        };
+        angular.forEach(mlist, function (value, key) {
+            if (typeof value !== "undefined") {
+                if (typeof txt === "undefined") {
+                    result[key] = value;
+                } else {
+                    if (value.toLowerCase().indexOf(txt.toLowerCase()) > -1) {
+                        result[key] = value;
+                    }
+                }
+            }
+        });
+        return result;
+    };
+    mtfctl.selectNode = function (list, txt, k, idx) {
         mtfctl.selected = k;
-        mtfctl.view=mtfctl.views[list];
-        mtfctl.selNode = node;
-        
+        $scope.selNode = {'data': list, 'index': idx};
+        mtfctl.view = mtfctl.views[list];
     };
     mtfctl.isSelected = function (k) {
         return mtfctl.selected === k;
     };
-
 });
-
-mtfApp.controller('usmsgCtl', function (dbService) {
-    var umsgctl = this;
-    umsgctl.usmsg = [];
-    dbService.getUIData('umsgs_ui', function (jsonres) {
-        umsgctl.usmsg=jsonres.Messages;
-    });
+mtfApp.controller('msgCtl', function ($scope, uiService, dbService) {
+    var msgctl = this;
 });
-mtfApp.controller('ussegCtl', function (dbService) {
+mtfApp.controller('segCtl', function ($scope, uiService, dbService) {
     var ussegctl = this;
-    ussegctl.usseg = [];
-    dbService.getUIData('usegments_ui', function (jsonres) {
-        ussegctl.usseg =jsonres.Segments;
-    });
 });
-mtfApp.controller('ussetCtl', function (dbService) {
+mtfApp.controller('setCtl', function ($scope, uiService, dbService) {
     var ussetctl = this;
-    ussetctl.usset = [];
-    dbService.getUIData('usets_ui', function (jsonres) {
-        ussetctl.usset =jsonres.Sets;
+});
+mtfApp.controller('fldCtl', function ($scope, uiService, dbService) {
+    var fldctl = this;
+    $scope.$watch("selNode", function () {
+        uiService.getUINode($scope.selNode.data, $scope.selNode.index, dbService.dB, function (node) {
+            fldctl.Field = node[0];
+            console.log(fldctl.Field);
+        });
     });
 });
-mtfApp.controller('usfldCtl', function (dbService) {
-    var usfldctl = this;
-    usfldctl.usfld = [];
-    dbService.getUIData('ufields_ui', function (jsonres) {
-        usfldctl.usfld =jsonres.Fields;
-    });
-});
-
-mtfApp.controller('natomsgCtl', function (dbService) {
-    var nmsgctl = this;
-    nmsgctl.natomsg = [];
-    dbService.getUIData('nmsgs_ui', function (jsonres) {
-        nmsgctl.natomsg =jsonres.Messages;
-    });
-});
-mtfApp.controller('natosegCtl', function (dbService) {
-    var nsegctl = this;
-    nsegctl.natoseg = [];
-    dbService.getUIData('nsegments_ui', function (jsonres) {
-        nsegctl.natoseg =jsonres.Segments;
-    });
-});
-mtfApp.controller('natosetCtl', function (dbService) {
-    var nsetctl = this;
-    nsetctl.natoset = [];
-    dbService.getUIData('nsets_ui', function (jsonres) {
-        nsetctl.natoset =jsonres.Sets;
-    });
-});
-mtfApp.controller('natofldCtl', function (dbService) {
-    var nfldctl = this;
-    nfldctl.natofld = [];
-    dbService.getUIData('nfields_ui', function (jsonres) {
-        nfldctl.natofld =jsonres.Fields;
-    });
-});
-
 mtfApp.controller('menuCtrl', function ($scope) {
     //initiate an array to hold all active tabs
     $scope.activeTabs = [];
--- MTFApp/mtfapp/js/services.js
+++ MTFApp/mtfapp/js/services.js
@@ -5,107 +5,62 @@ mtfApp.factory('dbService', function ($http, $indexedDB) {
     var dbsvc = {
     };
     dbsvc.resources = {
-        usmtf_msgs: '/xml/lz/usmtf_messages.xsd.lz',
-        usmtf_segs: '/xml/lz/usmtf_segments.xsd.lz',
-        usmtf_sets: '/xml/lz/usmtf_sets.xsd.lz',
-        usmtf_flds: '/xml/lz/usmtf_fields.xsd.lz',
-        nato_msgs: '/xml/lz/nato_messages.xsd.lz',
-        nato_segs: '/xml/lz/nato_segments.xsd.lz',
-        nato_sets: '/xml/lz/nato_sets.xsd.lz',
-        nato_flds: '/xml/lz/nato_fields.xsd.lz',
-        umsgs_ui: '/xml/lz/usmtf_messages_ui.xml.lz',
+//        usmtf_msgs: '/xml/lz/usmtf_messages.xsd.lz',
+//        usmtf_segs: '/xml/lz/usmtf_segments.xsd.lz',
+//        usmtf_sets: '/xml/lz/usmtf_sets.xsd.lz',
+//        usmtf_flds: '/xml/lz/usmtf_fields.xsd.lz',
+//        nato_msgs: '/xml/lz/nato_messages.xsd.lz',
+//        nato_segs: '/xml/lz/nato_segments.xsd.lz',
+//        nato_sets: '/xml/lz/nato_sets.xsd.lz',
+//        nato_flds: '/xml/lz/nato_fields.xsd.lz',
+//        umsgs_ui: '/xml/lz/usmtf_messages_ui.xml.lz',
         usets_ui: '/xml/lz/usmtf_sets_ui.xml.lz',
-        usegments_ui: '/xml/lz/usmtf_segments_ui.xml.lz',
+//        usegments_ui: '/xml/lz/usmtf_segments_ui.xml.lz',
         ufields_ui: '/xml/lz/usmtf_fields_ui.xml.lz',
-        nmsgs_ui: '/xml/lz/nato_messages_ui.xml.lz',
+//        nmsgs_ui: '/xml/lz/nato_messages_ui.xml.lz',
         nsets_ui: '/xml/lz/nato_sets_ui.xml.lz',
-        nsegments_ui: '/xml/lz/nato_segments_ui.xml.lz',
+//        nsegments_ui: '/xml/lz/nato_segments_ui.xml.lz',
         nfields_ui: '/xml/lz/nato_fields_ui.xml.lz'
     };
 
-    dbsvc.xj = new X2JS();
     dbsvc.dB = $indexedDB;
-    dbsvc.saxonloaded = false;
-    dbsvc.xslproc = null;
 
-    dbsvc.onSaxonLoad = function () {
-        console.log('saxon loaded');
-        dbsvc.saxonloaded = true;
-        dbsvc.xslproc = Saxon.newXSLT20Processor();
-    };
-
-    dbsvc.getUIData = function (uidata, callback) {
-        console.log('getUIData ' + uidata);
-        dbsvc.dB.openStore('MTF', function (store) {
-            store.getAllKeys().then(function (keys) {
-                if (keys.indexOf(uidata) === -1) {
-                    dbsvc.getResource(uidata, function (xrslt) {
-                        dbsvc.toJson(xrslt.data, function (jrslt) {
-                            callback(jrslt);
-                            dbsvc.dB.openStore('MTF', function (mtfstore) {
-                                mtfstore.upsert({name: uidata, lastmod: xrslt.lastmod, data: jrslt});
-                            });
-                        });
+    dbsvc.syncResources = function (uiService,mtfctl,callback) {
+        console.log("syncResources " + Object.keys(dbsvc.resources).length);
+        var count = 0;
+        for (var k in dbsvc.resources) {
+            //console.log("load " + k);
+            dbsvc.getResource(k, function (res) {
+                if (res['name'].substring(res['name'].length - 2) === 'ui') {
+                    uiService.synchUIData(res['name'], res.data, res.lastmod, dbsvc.dB, function (uiname,uidata) {
+                        mtfctl[uiname]=jsonPath(uidata, '$.*.*.*.Info._name');
+                       count++;
+                        if (count === Object.keys(dbsvc.resources).length) {
+                            callback();
+                        }
                     });
                 } else {
-                    store.find(uidata).then(function (uidbrec) {
-                        dbsvc.dB.openStore('Resources', function (resstore) {
-                            resstore.find(uidata).then(function (resdbrec) {
-                                if (resdbrec.lastmod !== uidbrec.lastmod) {
-                                    dbsvc.getResource(uidata, function (xrslt) {
-                                        dbsvc.toJson(xrslt.data, function (jrslt) {
-                                            callback(jrslt);
-                                            dbsvc.dB.openStore('MTF', function (mtfstore) {
-                                                mtfstore.upsert({name: uidata, lastmod: xrslt.lastmod, data: jrslt});
-                                            });
-                                        });
-                                    });
-                                } else {
-                                    callback(uidbrec.data);
-                                }
-                            });
-                        });
-
-                    });
+                    count++;
+                    if (count === Object.keys(dbsvc.resources).length) {
+                        callback();
+                    }
                 }
             });
-        });
-    };
-
-    dbsvc.uiXSL = function (xslname, xmlsrc, callback) {
-        console.log('uiXSL ' + xslname + ' ' + xmlsrc);
-        dbsvc.getResource(xslname, function (xslres) {
-            var xslstr = xslres.data;
-            dbsvc.getResource(xmlsrc, function (xmlres) {
-                var xmlstr = xmlres.data;
-                dbsvc.xslTransform(xslstr, xmlstr, function (xrslt) {
-                    dbsvc.toJson(xrslt, function (jrslt) {
-                        if (jrslt !== null) {
-                            callback(jrslt);
-                            dbsvc.dB.openStore('MTF', function (mtfstore) {
-                                mtfstore.upsert({name: xmlsrc, lastmod: xmlres.lastmod, data: jrslt});
-                            });
-                        } else {
-                            console.log("Error with transform");
-                        }
-                    })
-                })
-            });
-        });
+        }
     };
 
     dbsvc.getResource = function (resource, callback) {
-        console.log('getResource ' + resource);
+        //console.log('getResource ' + resource);
         var xname = resource;
         var xurl = dbsvc.resources[resource];
         dbsvc.dB.openStore('Resources', function (store) {
             store.getAllKeys().then(function (keys) {
                 if (keys.indexOf(xname) === -1) {
-                    console.log("No Local Store .. Add: " + xurl);
+                    //console.log("No Local Store .. Add: " + xurl);
                     $http.get(xurl).success(function (resdata, status, headers) {
                         var mod = headers()['last-modified'];
                         dbsvc.decompress(resdata, function (dcmp) {
-                            callback({'data': dcmp, 'lastmod': mod});
+                            callback({'name': xname, 'data': dcmp, 'lastmod': mod});
                         });
                         dbsvc.dB.openStore('Resources', function (store) {
                             store.upsert({name: xname, url: xurl, lastmod: mod, data: resdata});
@@ -122,7 +77,7 @@ mtfApp.factory('dbService', function ($http, $indexedDB) {
                                 console.log(xname + "Modified ..Update");
                                 $http.get(xurl).success(function (resdata, status, headers) {
                                     dbsvc.decompress(resdata, function (dcmp) {
-                                        callback({'data': dcmp, 'lastmod': mod});
+                                        callback({'name': xname, 'data': dcmp, 'lastmod': mod});
                                     });
                                     dbsvc.dB.openStore('Resources', function (store) {
                                         store.upsert({name: xname, url: xurl, lastmod: mod, data: resdata});
@@ -132,8 +87,8 @@ mtfApp.factory('dbService', function ($http, $indexedDB) {
                                 });
                             } else {
                                 dbsvc.decompress(dbdata, function (dcmp) {
-                                    console.log('Return cached ');
-                                    callback({'data': dcmp, 'lastmod': mod});
+                                    //console.log('Return cached ');
+                                    callback({'name': xname, 'data': dcmp, 'lastmod': mod});
                                 })
                             }
                         }).error(function () {
@@ -146,25 +101,159 @@ mtfApp.factory('dbService', function ($http, $indexedDB) {
         return dbsvc;
     };
 
-    dbsvc.xslTransform = function (xslstr, xmlstr, callback) {
-        dbsvc.xslproc = Saxon.newXSLT20Processor(Saxon.parseXML(xslstr));
-        var xslresult = dbsvc.xslproc.transformToFragment(Saxon.parseXML(xmlstr));
-        var uixml = Saxon.serializeXML(xslresult);
-        callback(uixml);
-    };
-
-    dbsvc.toJson = function (xdata, callback) {
-        var uijson = dbsvc.xj.xml_str2json(xdata);
-        callback(uijson);
-    };
-
     dbsvc.decompress = function (cdata, callback) {
         var d = LZString.decompressFromUTF16(cdata);
         callback(d);
     };
 
+    dbsvc.compress = function (udata, callback) {
+        var c = LZString.compressToUTF16(udata);
+        callback(c);
+    };
+
     return dbsvc;
 
+});
+
+mtfApp.factory('uiService', function ($http) {
+    var uisvc = {
+    };
+    uisvc.xslproc = [];
+    uisvc.saxonloaded = false;
+    uisvc.xslproc = null;
+
+    uisvc.onSaxonLoad = function () {
+        console.log('saxon loaded');
+        uisvc.saxonloaded = true;
+    };
+
+    uisvc.xj = new X2JS();
+
+    uisvc.synchUIData = function (uiname, uidata, lastmod, IDB, callback) {
+        //console.log('synchUIData ' + uiname);
+        IDB.openStore('MTF', function (store) {
+            store.getAllKeys().then(function (keys) {
+                if (keys.indexOf(uiname) === -1) {
+                    uisvc.toJson(uidata, function (jrslt) {
+                        IDB.openStore('MTF', function (mtfstore) {
+                            mtfstore.upsert({name: uiname, lastmod: lastmod, data: jrslt}).then(function () {
+                                callback(uiname,jrslt);
+                            });
+                        });
+                    });
+                } else {
+                    store.find(uiname).then(function (uidbrec) {
+                        if (uidbrec.lastmod !== lastmod) {
+                            uisvc.toJson(uidata, function (jrslt) {
+                                IDB.openStore('MTF', function (mtfstore) {
+                                    mtfstore.upsert({name: uiname, lastmod: lastmod, data: jrslt}).then(function () {
+                                        callback(uiname,jrslt);
+                                    });
+                                });
+                            });
+                        } else {
+                            //console.log('local data not modified');
+                            callback(uiname,uidbrec.data);
+                        }
+                    });
+
+                }
+            });
+        });
+    };
+
+    uisvc.getUIList = function (uiname, IDB, callback) {
+        IDB.openStore('MTF', function (store) {
+            store.find(uiname).then(function (uijson) {
+                var uilist = jsonPath(uijson.data, '$.*.*.*.Info._name');
+                callback(uilist);
+            });
+        });
+    };
+    
+    uisvc.getUINode = function (uiname, index,IDB, callback) {
+        var jpath='$.*.*['+index+']';
+        IDB.openStore('MTF', function (store) {
+            store.find(uiname).then(function (uijson) {
+                var uinode = jsonPath(uijson.data, jpath);
+                callback(uinode);
+            });
+        });
+    };
+
+    uisvc.getUIData = function (uidata, dbService, callback) {
+        console.log('getUIData ' + uidata);
+        dbService.dB.openStore('MTF', function (store) {
+            store.getAllKeys().then(function (keys) {
+                if (keys.indexOf(uidata) === -1) {
+                    dbService.getResource(uidata, function (xrslt) {
+                        uisvc.toJson(xrslt.data, function (jrslt) {
+                            callback(jrslt);
+                            dbService.dB.openStore('MTF', function (mtfstore) {
+                                mtfstore.upsert({name: uidata, lastmod: xrslt.lastmod, data: jrslt});
+                            });
+                        });
+                    });
+                } else {
+                    store.find(uidata).then(function (uidbrec) {
+                        dbService.dB.openStore('Resources', function (resstore) {
+                            resstore.find(uidata).then(function (resdbrec) {
+                                if (resdbrec.lastmod !== uidbrec.lastmod) {
+                                    dbService.getResource(uidata, function (xrslt) {
+                                        uisvc.toJson(xrslt.data, function (jrslt) {
+                                            callback(jrslt);
+                                            dbService.dB.openStore('MTF', function (mtfstore) {
+                                                mtfstore.upsert({name: uidata, lastmod: xrslt.lastmod, data: jrslt});
+                                            });
+                                        });
+                                    });
+                                } else {
+                                    callback(uidbrec.data);
+                                }
+                            });
+                        });
+
+                    });
+                }
+            });
+        });
+    };
+
+    uisvc.uiXSL = function (xslname, xmlsrc, dbService, callback) {
+        console.log('uiXSL ' + xslname + ' ' + xmlsrc);
+        uisvc.getResource(xslname, function (xslres) {
+            var xslstr = xslres.data;
+            dbService.getResource(xmlsrc, function (xmlres) {
+                var xmlstr = xmlres.data;
+                uisvc.xslTransform(xslstr, xmlstr, function (xrslt) {
+                    uisvc.toJson(xrslt, function (jrslt) {
+                        if (jrslt !== null) {
+                            callback(jrslt);
+                            dbService.dB.openStore('MTF', function (mtfstore) {
+                                mtfstore.upsert({name: xmlsrc, lastmod: xmlres.lastmod, data: jrslt});
+                            });
+                        } else {
+                            console.log("Error with transform");
+                        }
+                    })
+                })
+            });
+        });
+    };
+
+    uisvc.xslTransform = function (xslstr, xmlstr, callback) {
+        uisvc.xslproc = Saxon.newXSLT20Processor(Saxon.parseXML(xslstr));
+        var xslresult = uisvc.xslproc.transformToFragment(Saxon.parseXML(xmlstr));
+        var uixml = Saxon.serializeXML(xslresult);
+        callback(uixml);
+    };
+
+    uisvc.toJson = function (xdata, callback) {
+        var uijson = uisvc.xj.xml_str2json(xdata);
+        callback(uijson);
+    };
+
+    return uisvc;
 });
 
 
